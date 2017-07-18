826f9278e5468f6307093eab8fc8c9b6da712042
AkshGautam <gaksh007@gmail.com>
2017-07-18 15:39:23 +0530

    preprocessing done

--- parse_email.py
+++ parse_email.py
@@ -1,7 +1,7 @@
 from nltk.stem.snowball import SnowballStemmer
 import string
 import email.parser 
-
+import re,cgi
 
 def parseText(f):
 
@@ -20,9 +20,21 @@ def parseText(f):
 	email_data=sub+payload
 
 	if len(email_data)>1:
+		email_data= re.sub(r'(https|http)?:\/\/(\w|\.|\/|\?|\=|\&|\%)*\b', '',email_data, flags=re.MULTILINE)
+ 		tag_re = re.compile(r'(<!--.*?-->|<[^>]*>)')
+ 		no_tags = tag_re.sub('',email_data)
+ 		email_data = cgi.escape(no_tags)
 		email_data=email_data.translate(string.maketrans("",""),string.punctuation)
-		stemmer=SnowballStemmer("english")
-		stemmed_words=[stemmer.stem(i) for i in email_data.split()]
-		email_data=' '.join(stemmed_words)
+		# to remove excess whitespaces
+		' '.join(email_data.split())
+		email_data.strip()
+		# To check the string which in this case is not as expected /x93 instead of punctuation removal
+		# lis=email_data.split()
+		# print lis
+		# stemmer not working cos the above code is not removing punctuation
+		# stemmer=SnowballStemmer("english")
+		# stemmed_words=[stemmer.stem(i) for i in email_data.split()]
+		# email_data=' '.join(stemmed_words)
+		# print email_data
 
-		print email_data
+	return email_data
--- vectorize.py
+++ vectorize.py
@@ -4,16 +4,32 @@ import sys
 
 from parse_email import parseText
 
-curr_path=os.getcwd()
-path="/home/aksh/web_app/Sweeper/CSDMC2010_SPAM/CSDMC2010_SPAM/TRAINING"
-email_path=os.chdir(path)
-email_list=os.listdir(path)
-print email_list[0]
+# curr_path=os.getcwd()
+# path=""
+# email_path=os.chdir(path)
+# email_list=os.listdir(path)
+# print email_list[0]
+path=os.getcwd()
+path=path+"/CSDMC2010_SPAM/CSDMC2010_SPAM/TRAINING/"
+fr=open("SPAMTrain.label","r")
 
 email_data=[]
-from_data=[]
+label_data=[]
+ct=0
+for x in fr:
+	col=x.split()
+	file_name=col[1]
+	file_location=os.path.join(path,file_name)
+	print "processing",file_name
+	email=open(file_location,"r")
+	email_text=parseText(email)
+	# print email_text
+	label_data.append(col[0])
+	email_data.append(email_text)
+	# print fr
+	email.close()
+print "Email processed"
+fr.close()
 
-# for i in email_list:
-
-email=open(email_list[0])
-email_text=parseText(email)
+# email=open(email_list[0])
+# email_text=parseText(email)
